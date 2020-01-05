8abf21b45c964d418e46bd77caae6d1e0a9eb8f6
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2020-01-05 12:09:15 -0500

    changing express port

--- ubuntu-node-dungeon/files/level1/express.js
+++ ubuntu-node-dungeon/files/level1/express.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 // express configuration
 app.use(express.json({type: '*/*'}));
